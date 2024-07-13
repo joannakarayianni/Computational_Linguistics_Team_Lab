7cc15bd2afcf724f62c6df9d1bbf032d4aa3d9a0
Tonmoy Rakshit <tonmoyrakshit@Tonmoys-MacBook-Air.local>
2024-07-13 20:04:06 +0200

    Added Precison, Recall, F-Score attributes for vaulting

--- scripts/advanced_classifier/lstm/vaulting.py
+++ scripts/advanced_classifier/lstm/vaulting.py
@@ -3,19 +3,19 @@ LSTM with Tf-Idf embeddings & LSTM with GloVe embeddings, (predictions were on t
 labels on each instance ) 
 The metrics used where F1 micro, F1 macro, Precision micro, recal micro and hamming loss"""
 import pandas as pd
-import numpy as np
 from sklearn.metrics import hamming_loss, f1_score, precision_score, recall_score
+from sklearn.metrics import precision_recall_fscore_support
 import matplotlib.pyplot as plt
 
 # Loading prediction files for all the models
-lstm_word_embeddings = pd.read_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/predicions.csv')
-bi_lstm_word_embeddings = pd.read_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/predictionsbilstm.csv')
-lstm_bert = pd.read_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/predictionsbert.csv')
-lstm_tfidf = pd.read_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/predictionstfidf.csv')
-lstm_glove = pd.read_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/predictionsglove.csv')
+lstm_word_embeddings = pd.read_csv('scripts/advanced_classifier/lstm/predicions.csv')
+bi_lstm_word_embeddings = pd.read_csv('scripts/advanced_classifier/lstm/predictionsbilstm.csv')
+lstm_bert = pd.read_csv('scripts/advanced_classifier/lstm/predictionsbert.csv')
+lstm_tfidf = pd.read_csv('scripts/advanced_classifier/lstm/predictionstfidf.csv')
+lstm_glove = pd.read_csv('scripts/advanced_classifier/lstm/predictionsglove.csv')
 
 # Golden standard (test data)
-ground_truth = pd.read_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/ground_truth.csv')
+ground_truth_test = pd.read_csv('scripts/evaluation/ground_truths/ground_truth_test.csv')
 
 # Models list
 models = {
@@ -27,46 +27,42 @@ models = {
 }
 
 # Storing performance metrics
-metrics = pd.DataFrame(columns=['Model', 'Emotion', 'Hamming Loss', 'F1 Micro', 'F1 Macro', 'Precision Micro', 'Recall Micro'])
+metrics = pd.DataFrame(columns=['Model', 'Emotion', 'Hamming Loss', 'F1 Score', 'Precision', 'Recall', 'F1 Micro', 'F1 Macro', 'Precision Micro', 'Recall Micro'])
 
 # Metrics for each model and each emotion separately
 for model_name, predictions in models.items():
-    for emotion in ground_truth.columns:  # Iterating over all columns
-        if emotion == 'index':  
-            continue
-        
-        y_true = ground_truth[emotion]
-        y_pred = predictions[emotion]
+        for emotion in ground_truth_test.columns:  # Iterating over all columns
+            
+            y_true = ground_truth_test[emotion]
+            y_pred = predictions[emotion]
 
-        # Dropping rows with NaN values
-        non_nan_indices = y_pred.dropna().index
-        y_true = y_true.loc[non_nan_indices]
-        y_pred = y_pred.loc[non_nan_indices]
+            # Using average='binary' focuses the metric calculations on the positive class in binary classification tasks
+            precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average='binary')
+            ham_loss = hamming_loss(y_true, y_pred)
+            f1_micro = f1_score(y_true, y_pred, average='micro')
+            f1_macro = f1_score(y_true, y_pred, average='macro')
+            precision_micro = precision_score(y_true, y_pred, average='micro')
+            recall_micro = recall_score(y_true, y_pred, average='micro')
 
-        ham_loss = hamming_loss(y_true, y_pred)
-        f1_micro = f1_score(y_true, y_pred, average='micro')
-        f1_macro = f1_score(y_true, y_pred, average='macro')
-        precision_micro = precision_score(y_true, y_pred, average='micro')
-        recall_micro = recall_score(y_true, y_pred, average='micro')
-        
-        metrics = metrics._append({
-            'Model': model_name,
-            'Emotion': emotion,
-            'Hamming Loss': ham_loss,
-            'F1 Micro': f1_micro,
-            'F1 Macro': f1_macro,
-            'Precision Micro': precision_micro,
-            'Recall Micro': recall_micro
-        }, ignore_index=True)
+            metrics = metrics._append({
+                'Model': model_name,
+                'Emotion': emotion,
+                'Hamming Loss': ham_loss,
+                'Precision': precision,
+                'Recall': recall,
+                'F1 Score': f1,
+                'F1 Micro': f1_micro,
+                'F1 Macro': f1_macro,
+                'Precision Micro': precision_micro,
+                'Recall Micro': recall_micro
+            }, ignore_index=True)
+
 
 # Saving the metrics 
-metrics.to_csv('/Users/ioannakaragianni/Documents/GitHub/Computational_Linguistics_Team_Lab/scripts/advanced_classifier/lstm/metrics.csv', index=False)
-
+metrics.to_csv('scripts/evaluation/all_results/evaluation_metrics.csv', index=False)
 
 # Visualization for each emotion
-for emotion in ground_truth.columns:
-    if emotion == 'index':  # Skip the index column assuming it's not an emotion
-        continue
+for emotion in ground_truth_test.columns:
     
     emotion_metrics = metrics[metrics['Emotion'] == emotion]
     
@@ -75,12 +71,27 @@ for emotion in ground_truth.columns:
     plt.ylabel('Hamming Loss')
     plt.show()
 
-    # Bar plot for F1 Scores
-    emotion_metrics.plot(x='Model', y=['F1 Micro', 'F1 Macro'], kind='bar', title=f'F1 Scores for {emotion} by Model')
-    plt.ylabel('F1 Score')
+    # Bar plot for Precision
+    emotion_metrics.plot(x='Model', y='Precision', kind='bar', title=f'Precision {emotion} by Model', legend=False)
+    plt.ylabel('Precision')
     plt.show()
 
-    # Bar plot for Precision and Recall
-    emotion_metrics.plot(x='Model', y=['Precision Micro', 'Recall Micro'], kind='bar', title=f'Precision and Recall for {emotion} by Model')
-    plt.ylabel('Score')
+    # Bar plot for Recall
+    emotion_metrics.plot(x='Model', y='Recall', kind='bar', title=f'Recall {emotion} by Model', legend=False)
+    plt.ylabel('Recall')
+    plt.show()
+
+    # Bar plot for F1 score
+    emotion_metrics.plot(x='Model', y='F1 Score', kind='bar', title=f'F1 score {emotion} by Model', legend=False)
+    plt.ylabel('F1 score')
+    plt.show()
+
+    # Bar plot for F1  Micro and Macro Scores
+    emotion_metrics.plot(x='Model', y=['F1 Micro', 'F1 Macro'], kind='bar', title=f'F1 Micro and Macro Scores for {emotion} by Model')
+    plt.ylabel('F1 Micro & Macro Scores')
+    plt.show()
+
+    # Bar plot for Precision and Recall Micros
+    emotion_metrics.plot(x='Model', y=['Precision Micro', 'Recall Micro'], kind='bar', title=f'Precision Micro and Recall Micro for {emotion} by Model')
+    plt.ylabel('Precision Micro and Recall Micro')
     plt.show()
--- scripts/dataloader/dataloader.py
+++ scripts/dataloader/dataloader.py
@@ -4,8 +4,9 @@ import pandas as pd
 class DataLoader:
 
     def __init__(self, path_to_train, path_to_val, path_to_test):
-        # load data
-        # For training and testing
+        
+        self.emotions = ["joy", "fear", "shame", "disgust", "guilt", "anger", "sadness"]
+
         self.df_train = self.load_data_from_csv_file(path_to_train)
 
         self.df_val = self.load_data_from_csv_file(path_to_val)
@@ -17,6 +18,11 @@ class DataLoader:
         self.y_val_labels = self.get_y_labels(self.df_val)
 
         self.y_test_labels = self.get_y_labels(self.df_test)
+
+        self.generate_ground_truths(self.df_test, "test")
+
+        self.generate_ground_truths(self.df_val, "val")
+
 
     """
         read the file and return the dataframe
@@ -47,3 +53,26 @@ class DataLoader:
 
     def get_y_labels(self, df):
         return df.iloc[:, 0]
+    
+    """
+        Generate ground truths for validation and test sets in binary format. 
+        Presence of an emotion is indicated by a 1 in the corresponding emotion column.
+    """
+
+    def generate_ground_truths(self, df, filename):
+        # empty DataFrame with the required structure
+
+        ground_truth_binary = pd.DataFrame(0, index=df.index, columns=self.emotions)
+
+        # Populating the DataFrame
+        for idx, row in df.iterrows():
+            emotion = row[0]
+            if emotion in self.emotions:
+                ground_truth_binary.at[idx, emotion] = 1
+        
+        # Save to a new CSV file
+        ground_truth_binary.to_csv(f'scripts/evaluation/ground_truths/ground_truth_{filename}.csv', index=False)
+
+
+
+
